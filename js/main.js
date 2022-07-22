8df42181d7fcdeec3f46496a74466a2a49f301ad
Yuuki <52652158+rene-roid@users.noreply.github.com>
2022-07-22 14:23:00 +0000

    Updates

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 import '../styles/style.css';
 
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
+// import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
 
 // Varibles
 const clock = new THREE.Clock();
