2ce6f0230f63dcc869b7098d052243d5304ca0cf
Yuuki <52652158+rene-roid@users.noreply.github.com>
2022-07-22 18:50:44 +0000

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Imports:
-import '../styles/style.css';
+import '../../dist/styles/style.css';
 
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 // import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
